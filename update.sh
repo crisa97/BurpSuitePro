ca89712b8c1f8e4d6ba6d71b75d5118907d1ff07
Crisa97 <cccruz76382@umanizales.edu.co>
2025-09-24 12:49:16 -0500

    update burp

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 BURP_DIR="/usr/share/burpsuitepro"
 BURP_SCRIPT="burpsuitepro"
 DOWNLOAD="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar&version=&"
-navegador="$HOME/BurpSuite-Pro/Linux/navegador.sh"
+navegador="$HOME/BurpSuitePro/Linux/navegador.sh"
 
 print_status() {
     echo -e "\e[1;34m$1\e[0m"
