92d75f77ce079e720afe1448c501186d1a5106ef
Crisa97 <cccruz76382@umanizales.edu.co>
2025-09-24 18:56:34 -0500

    update scritp for uptate brup

--- update.sh
+++ update.sh
@@ -4,7 +4,8 @@
 BURP_DIR="/usr/share/burpsuitepro"
 BURP_SCRIPT="burpsuitepro"
 DOWNLOAD="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar&version=&"
-navegador="$HOME/BurpSuitePro/Linux/navegador.sh"
+navegador="$HOME/BurpSuitePro/navegador.sh"
+ROUTE="$HOME/Descargas"
 
 print_status() {
     echo -e "\e[1;34m$1\e[0m"
@@ -13,7 +14,7 @@ print_status() {
 upgrad_burpsuite() {
     print_status 'Upgrading Burp Suite Professional... :)'
     bash $navegador $DOWNLOAD . >/dev/null 2>&1
-    mv *.jar burpsuite_pro.jar && sudo mv burpsuite_pro.jar $BURP_DIR
+    mv $ROUTE/*.jar $ROUTE/burpsuite_pro.jar && sudo mv $ROUTE/burpsuite_pro.jar $BURP_DIR || { echo "Failed to launch Burp Suite!"; exit 1; }
     print_status "Burp Suite successfully updated :D"
 }
 
